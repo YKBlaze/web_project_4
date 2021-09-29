be989229aaa70b0e0bd6d48c2baaf3ab276b60bb
Yol <yol135@gmail.com>
2021-09-29 04:22:49 +0300

    project upload

--- scripts/script.js
+++ scripts/script.js
@@ -11,14 +11,16 @@ const modals = [
     identifier: "edit-profile",
     inputName: "Name",
     inputValue: "About-me",
-    error: "Characters limit exceeded"
+    error: "Characters limit exceeded",
+    submit: "Save"
   },
   {
     title: "New Place",
     identifier: "new-place",
     inputName: "Title",
     inputValue: "Image URL",
-    error: "Invalid title or URL"
+    error: "Invalid title or URL",
+    submit: "Create"
   }
 ];
 const initialCards = [
@@ -47,7 +49,7 @@ const initialCards = [
       link: "https://code.s3.yandex.net/web-code/lago.jpg"
     }
   ];
-function generateModals(){
+function initiateModals(){
   for (let i = 0; i <modals.length; i++)
   {
     const modalTemplate = document.querySelector(`#modal`).content;
@@ -64,13 +66,30 @@ function generateModals(){
     modalInputValue.textContent = modals[i].inputValue;
     modalInputName.placeholder = modals[i].inputName;
     modalInputValue.placeholder = modals[i].inputValue;
+    modalButton.textContent = modals[i].submit;
     modal.id = "modal__" + modals[i].identifier;
     page.append(modal);
   }
 }
+function initiateImagePopUp(num){
+  const imagePopUpTemplate = document.querySelector(`#image-popup`).content;
+  const imagePopUp = imagePopUpTemplate.querySelector('.image-popup').cloneNode(true);
+  imagePopUp.querySelector('.image-popup__image').src = initialCards[num].link;
+  imagePopUp.querySelector('.image-popup__footer').textContent = initialCards[num].name;
+  imagePopUp.querySelector('.image-popup__close').disabled = true;
+  imagePopUp.querySelector('.image-popup__close').addEventListener('click', (evt) => {
+    imagePopUp.classList.add('image-popup_disabled');
+    pageOverlay.classList.add('page__overlay_disabled');
+    imagePopUp.querySelector('.image-popup__close').disabled = true;
+  })
+  imagePopUp.id = "image-popup-" + (num+1);
+  return page.prepend(imagePopUp);
+}
 function initiateCard(num){
     const elementTemplate = document.querySelector(`#element`).content;
     const elementCard = elementTemplate.querySelector('.element').cloneNode(true);
+    const identifier = "#image-popup-" + (num+1);
+    initiateImagePopUp(num);
     elementCard.id = "element-" + (num + 1);
     elementCard.querySelector(`.element__title`).textContent = initialCards[num].name;
     elementCard.querySelector('.element__image').src = initialCards[num].link;
@@ -78,12 +97,16 @@ function initiateCard(num){
       const eventTarget = evt.target;
       eventTarget.classList.toggle(`element__like-button_active`);
       });
-    elementCard.querySelector('.element__delete-button').addEventListener('click',(evt) =>{
+    elementCard.querySelector('.element__delete-button').addEventListener('click',() =>{
       elementCard.remove();
     })
-    elementCard.querySelector('.element__image-button').addEventListener('click',(evt =>{
-      console.log(evt);
-    }))
+    elementCard.querySelector('.element__image-button').addEventListener('click',(evt) =>{
+      const imagePopUp = document.querySelector(identifier);
+      imagePopUp.classList.remove('image-popup_disabled');
+      imagePopUp.querySelector('.image-popup__close').disabled = false;
+      pageOverlay.classList.remove('page__overlay_disabled');
+    });
+    
     return elements.prepend(elementCard);
 }
 function initiateCards(){
@@ -92,10 +115,16 @@ function initiateCards(){
     initiateCard(i);
    }
 }
-function imageOpen(){
-  console.log()
+function handleEnter(evt) {
+  const modalSave = document.querySelector('.modal__save');
+    if (evt.keyCode === 13) {
+      evt.preventDefault();
+      modalSave.click();
+      console.log(page);
+      page.removeEventListener('keyup', handleEnter);
+    }
 }
-modalEditButton.addEventListener('click',(evt) => {
+function openEditForm(evt){
   const modalEditOpen = document.querySelector(`#modal__edit-profile`);
   const modalClose = modalEditOpen.querySelector('.modal__close');
   const modalSave = modalEditOpen.querySelector('.modal__save');
@@ -107,6 +136,7 @@ modalEditButton.addEventListener('click',(evt) => {
   modalEditOpen.classList.remove('modal_disabled');
   modalName.value = profileName.textContent;
   modalAboutMe.value = profileAboutMe.textContent;
+  page.addEventListener('keyup', handleEnter);
   modalClose.addEventListener('click',(evt) => {modalEditOpen.classList.add('modal_disabled'); pageOverlay.classList.add('page__overlay_disabled'); modalSave.disabled = true;});
   modalSave.addEventListener('click', function handleEditSubmit(evt) {
     evt.preventDefault();
@@ -121,14 +151,9 @@ modalEditButton.addEventListener('click',(evt) => {
     return modalError.classList.remove('modal__error_disabled');
     }
   });
-  modalSave.addEventListener('keyup', (evt) => {
-    if (evt.keyCode === 13) {
-      evt.preventDefault();
-      modalSave.click();
-    }
-  })
-});
-modalAddButton.addEventListener('click',(evt) =>{
+
+}
+function openCreateForm(evt) {
   const modalAddOpen = document.querySelector(`#modal__new-place`);
   pageOverlay.classList.remove('page__overlay_disabled');
   const modalClose = modalAddOpen.querySelector('.modal__close');
@@ -136,14 +161,16 @@ modalAddButton.addEventListener('click',(evt) =>{
   const modalName = modalAddOpen.querySelector(`.modal__name`);
   const modalAboutMe = modalAddOpen.querySelector(`.modal__about-me`);
   const modalError = modalAddOpen.querySelector(`.modal__error`);
+  modalName.value = "";
+  modalAboutMe.value = "";
   modalError.classList.add('modal__error_disabled');
   modalSave.disabled = false;
   modalAddOpen.classList.remove('modal_disabled');
   modalClose.addEventListener('click',(evt) => {modalAddOpen.classList.add('modal_disabled'); pageOverlay.classList.add('page__overlay_disabled'); modalSave.disabled = true;});
   modalSave.addEventListener('click', function handleAddSubmit(evt) {
     evt.preventDefault(); 
-    if (modalName.value.length > 0 && modalAboutMe.value.length > 0) {
-          const initialCardsUpdated = [
+    if (modalName.value.length > 0 && modalName.value.length < 70 && modalAboutMe.value.length > 0) {
+       const initialCardsUpdated = [
       {
         name: "",
         link: ""
@@ -162,6 +189,8 @@ modalAddButton.addEventListener('click',(evt) =>{
     }
     evt.stopImmediatePropagation();
   });
-});
+}
 initiateCards();
-generateModals();
+initiateModals();
+modalEditButton.addEventListener('click', openEditForm);
+modalAddButton.addEventListener('click', openCreateForm);
